918a22509513f66f47c1736ac40bebd215112c2b
airotod <airotod@gmail.com>
2020-05-23 14:35:09 +0900

    Update header

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,22 @@ export default function Header() {
     <header className={styles.head}>
       <div className={styles.inner_head}>
         <div className={styles.wrap_head}>
-          <Link to="/">My Blog</Link>
+          <div className={styles.head_logo}>
+            <Link to="/">
+              <img src="/eyes.png" />
+            </Link>
+          </div>
+          <div className={styles.head_title}>
+            <Link to="/">My Blog</Link>
+          </div>
+          <ul className={styles.head_nav}>
+            <li>
+              <Link to="/music/">Music</Link>
+            </li>
+            <li>
+              <Link to="/book/">Book</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
